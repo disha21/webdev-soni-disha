a43ea25b075feb5d4a8649fa13f66b76a81300f1
Disha  Soni <dishasoni@Dishas-MacBook-Pro.local>
2016-12-01 00:02:58 -0500

    Home Profile page

---
+++ public/project/js/main.js
@@ -0,0 +1,12 @@
+/**
+ * Created by dishasoni on 11/29/16.
+ */
+
+$( document ).ready(function() {
+    console.log( "ready!" );
+
+
+$( function() {
+    $( "#datepicker" ).datepicker();
+});
+});
