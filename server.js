a16f7c19fd080b29f980417e1165625cc0f13ef1
Disha  Soni <dishasoni@Dishas-MacBook-Pro.local>
2017-10-25 19:18:42 -0400

    connection string

--- server.js
+++ server.js
@@ -44,6 +44,6 @@ mongoose.connect(connectionString);
 
 
 //var ipaddress = process.env.OPENSHIFT_NODEJS_IP;
-var port      = process.env.Port || 3000;
+var port = process.env.PORT || 3000;
 
 app.listen(port);
