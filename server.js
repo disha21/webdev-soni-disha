bdbc1f3fc63671be33bb38d3ddea9b6f84ef7b1c
Disha  Soni <dishasoni@Dishas-MacBook-Pro.local>
2017-10-25 18:42:50 -0400

    heroku changes

--- server.js
+++ server.js
@@ -25,22 +25,24 @@ app.use(express.static(__dirname + '/public'));
 require ("./project/app.js")(app);
 //require ("./assignment/app.js")(app);
 
-var connectionString = 'mongodb://127.0.0.1:27017/wam-fall-2016';
+//var connectionString = 'mongodb://127.0.0.1:27017/wam-fall-2016';
+var connectionString = 'mongodb://'+ process.env.HEROKU_MONGODB_DB_USERNAME + ":"
+    + process.env.HEROKU_MONGODB_DB_PASSWORD +'@ds031835.mlab.com:31835/dishadata'
 
-if(process.env.OPENSHIFT_MONGODB_DB_PASSWORD) {
-    connectionString = process.env.OPENSHIFT_MONGODB_DB_USERNAME + ":" +
-        process.env.OPENSHIFT_MONGODB_DB_PASSWORD + "@" +
-        process.env.OPENSHIFT_MONGODB_DB_HOST + ':' +
-        process.env.OPENSHIFT_MONGODB_DB_PORT + '/' +
-        process.env.OPENSHIFT_APP_NAME;
-}
+// if(process.env.OPENSHIFT_MONGODB_DB_PASSWORD) {
+//     connectionString = process.env.OPENSHIFT_MONGODB_DB_USERNAME + ":" +
+//         process.env.OPENSHIFT_MONGODB_DB_PASSWORD + "@" +
+//         process.env.OPENSHIFT_MONGODB_DB_HOST + ':' +
+//         process.env.OPENSHIFT_MONGODB_DB_PORT + '/' +
+//         process.env.OPENSHIFT_APP_NAME;
+// }
 
 console.log("Connection String : "  + connectionString);
 var mongoose = require("mongoose");
 mongoose.connect(connectionString);
 
 
-var ipaddress = process.env.OPENSHIFT_NODEJS_IP;
-var port      = process.env.OPENSHIFT_NODEJS_PORT || 3000;
+//var ipaddress = process.env.OPENSHIFT_NODEJS_IP;
+var port      = process.env.Port || 3000;
 
 app.listen(port, ipaddress);
